8dc0fb2ca2e1a573ed989773cb712dce8d8784ca
KRG0098 <kaustubh0098@gmail.com>
2025-03-13 21:00:27 +0530

    Add template for writing functions

---
+++ kNN_ASMR/Function_template.py
@@ -0,0 +1,59 @@
+####################################################################################################
+
+#-------------------  These libraries are required for evaluating the functions  -------------------
+
+import #<enter library name here>
+
+from kNN_ASMR.HelperFunctions import #<import helper package name here>
+
+####################################################################################################
+
+#--------------------------------------  Function Definitions  -------------------------------------
+
+def #<enter function name here>(<enter function arguments here>):
+
+    '''
+    <enter description of function here>
+
+    #-----------------------------------------------------------------------------------------------
+    
+    Parameters
+    ----------
+    
+    <enter parameter name>: <enter parameter type (int, float, array, etc.)>
+        <enter description of parameter here (also mention default values if any)>
+        
+    .
+    .
+    .
+        
+    <enter parameter name>: <enter parameter type (int, float, array, etc.)>
+        <enter description of parameter here (also mention default values if any)>
+
+    #-----------------------------------------------------------------------------------------------
+
+    Returns
+    -------
+
+    <enter output object name>: <enter type (int, float, array, etc.)>
+        <enter description of object here>
+        
+    .
+    .
+    .
+        
+    <enter output object name>: <enter type (int, float, array, etc.)>
+        <enter description of object here>
+    '''
+    
+    #-----------------------------------------------------------------------------------------------
+
+    #<enter function body here>
+    
+    return #<object name>, ..., <object name>
+
+####################################################################################################
+
+#----------------------------------------  END OF PROGRAM!  ----------------------------------------
+
+####################################################################################################
