c448203a6a5c5b57cdb0f0b53dc301818cdb4c19
Anargha Mondal <anargha.mondal@students.iiserpune.ac.in>
2025-05-22 17:22:59 +0530

    Added stashed chnages to 3DTPCF_Tracer-Field.py

--- kNN_ASMR/Auxiliary/TPCF/3DTPCF_Tracer-Field.py
+++ kNN_ASMR/Auxiliary/TPCF/3DTPCF_Tracer-Field.py
@@ -15,17 +15,13 @@ from HelperFunctions.py import smoothing_3D, create_query_3D
 
 # Function that returns the the 2-point Cross-Correlation Function between a set of discrete tracers and a continuous field using the stacking method
 
-def CrossCorr2pt(query_type, query_grid, bins, pos, delta, thickness, Filter, BoxSize, R=None, kmin=None, kmax=None, Verbose=False):
+def CrossCorr2pt(bins, pos, delta, thickness, Filter, BoxSize, R=None, kmin=None, kmax=None, Verbose=False):
     '''
     Calculates the Two-point Cross-correlation function between a set of tracers and a field. The interpolation can only be done using the 
     CIC-mass assignment scheme.
 
     Parameters
     ----------
-    query_type : {'grid', 'random'}, str
-        the type of query points to be generated; should be 'grid' for query points defined on a uniform grid and 'random' for query points drawn from a uniform random distribution.
-    query_grid : int
-        the 1D size of the query points array; the total number of query points generated will be ``query_grid**3``.
     bins : float array of shape (m,)
         Set of m radial distances at which the 2PCF will be computed
         
@@ -53,16 +49,15 @@ def CrossCorr2pt(query_type, query_grid, bins, pos, delta, thickness, Filter, Bo
         raise ValueError("Error: Input array is not cubical (n, n, n).")
     ngrid = shape[0]
 
+    grid = np.linspace(0, BoxSize, ngrid, endpoint=False)  # Grid points in each dimension
     smoothed_delta= smoothing_3D(delta, Filter, grid, BoxSize, R, kmin, kmax, thickness, Verbose)
-    # Interpolating the field at the tracer (galaxy) positions
     
-    grid=create_query_3D(query_type, query_grid, BoxSize)
     delta_interp = np.zeros((len(bins), len(pos)))  # Shape (number_of_bins, number_of_tracers)
 
     # Perform interpolation for each smoothed field
     for i, R in enumerate(bins):
         density_interpolated = np.zeros(pos.shape[0], dtype=np.float32)
-        MASL.CIC_interp(smoothed_delta[i], boxsize, pos, density_interpolated)
+        MASL.CIC_interp(smoothed_delta[i], BoxSize, pos, density_interpolated)
         delta_interp[i] = density_interpolated
 
     # Computing the 2-point Cross-Correlation Function by averaging over the interpolated field at the tracer positions
