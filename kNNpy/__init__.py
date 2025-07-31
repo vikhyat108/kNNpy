faa0b52d1a349e2e1b48a7d56313834c18c6a0e4
Anargha Mondal <anargha.mondal@students.iiserpune.ac.in>
2025-07-31 12:06:38 +0530

    some stuff written

--- kNNpy/__init__.py
+++ kNNpy/__init__.py
@@ -1,3 +1,22 @@
 '''
-kNNpy serves to provide a suite of high-performance routines to quickly calculate the higher-order statistics called k Nearest Neighbour Cumulative Distribution Functions (hereby referred to as k NN CDFs). k NN CDFs are sensitive to all N point correlation functions, are inexpensive to compute, and have been shown to be more powerful than the standard two-point correlation function. Refer to the papers mentioned here (https://kitnenikatnivasi.github.io/).
+"""
+<p align="center">
+  <img src="https://github.com/vikhyat108/kNNpy/blob/main/logo2.png" alt="kNNpy Logo" width="240"/>
+</p>
+
+> A modular Python toolkit for computing **$k$-nearest neighbour (kNN) distributions** — advanced clustering statistics that go beyond the traditional two-point correlation function.
+
+---
+
+## Overview
+
+`kNNpy` is a high-performance package for computing **$k$-nearest neighbour cumulative distribution functions (kNN CDFs)** — powerful summary statistics that are sensitive to **all connected $N$-point functions** of the matter density field.
+
+Unlike the standard two-point correlation function, kNN statistics capture **non-Gaussian information**, making them a valuable tool for cosmological inference using modern survey data.
+
+For theory, examples, and science behind the code, visit:  
+**[kitnenikatnivasi.github.io](https://kitnenikatnivasi.github.io)**
+
+---
+
 '''
