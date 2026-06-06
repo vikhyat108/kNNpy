e7afb05c64be143e662fc265efff7183dba64f0c
Anargha Mondal <anargha.mondal@students.iiserpune.ac.in>
2026-06-06 17:32:21 +0530

    Completed the Fisher tutorial nb, and added a feature of creating the ND Gaussian to the function

--- kNNpy/Auxiliary/Fisher.py
+++ kNNpy/Auxiliary/Fisher.py
@@ -1,12 +1,14 @@
 import numpy as np
+from getdist.gaussian_mixtures import GaussianND
 
-def constructingFishermatrix(cov_matrix, der_matrix, n_cov):
+
+def constructingFishermatrix(cov_matrix, der_matrix, n_cov, mean, labels):
     """
     Caluclated the Fisher information matrix given the covariance matrix and the derivative matrix. The covariance matrix is the covariance of the data, and the derivative matrix is the derivative of the data with respect to the parameters. The Fisher information matrix is given by the formula:
 
     Inputs:
     cov_matrix: The covariance matrix of the data. This should be a square matrix of shape (n, n), where n is the number of data points.
-    der_matrix: The derivative of the data with respect to the parameters. This should be a matrix of shape (n, m), where n is the number of data points and m is the number of parameters.
+    der_matrix: The derivative of the data with respect to the parameters. This should be a matrix of shape (m, n), where n is the number of data points and m is the number of parameters.
     n_cov: The number of realizations the covariance matrix is estimated from.
 
     Returns:
@@ -15,8 +17,13 @@ def constructingFishermatrix(cov_matrix, der_matrix, n_cov):
     # Calculating the hartlap factor
     hartlap = (n_cov - cov_matrix.shape[0] - 2) / (n_cov - 1)
     c_inv = hartlap*np.linalg.inv(cov_matrix)
-    F = np.zeros([der_matrix.shape[1], der_matrix.shape[1]])
-    for i in range(der_matrix.shape[1]):
-        for j in range(der_matrix.shape[1]):
+    F = np.zeros([der_matrix.shape[0], der_matrix.shape[0]])
+    for i in range(der_matrix.shape[0]):
+        for j in range(der_matrix.shape[0]):
             F[i][j] = (np.transpose(der_matrix[i])).dot(c_inv).dot(der_matrix[j])
-    return F
+
+    # Build an N-D Gaussian for the parameters
+    cov=np.linalg.inv(F)
+    gauss = GaussianND(mean, cov, labels=labels)
+
+    return F, gauss
